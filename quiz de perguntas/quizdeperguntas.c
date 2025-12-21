1ba3d7aa13bdc6041994fe0ecf4f145a80340a0b
domingos <domy.pinto.rodry@gmail.com>
2025-12-21 20:52:54 +0000

    adicao de menu fim de jogo

--- quiz de perguntas/quizdeperguntas.c
+++ quiz de perguntas/quizdeperguntas.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
 #include<locale.h>
 #include <string.h>
+void mostrarMenu(	char *pergunta[], int acertos, int erros, int i){
+		printf("==========QUIZ==========\n\n");
+		printf("Acertos: %d\n",acertos);
+		printf("Erros: %d\n",erros);
+		printf("Pergunta %d\n\n",i+1);
+		printf("%s\n", pergunta[i]);
+}
 main()
 {
 setlocale(LC_ALL,"Portuguese");
 	int i,j,resposta,acertos=0,erros=0;
+	char repetir;
 	char *pergunta[] ={
 	 "Qual é a capital de Portugal?",
 	 "Quanto é 5 + 3?",
@@ -47,28 +55,37 @@ setlocale(LC_ALL,"Portuguese");
 		"int",
 		"Repetir codigo",
 		};
-	for(i=0;i<=5;i++){
-		printf("==========QUIZ==========\n\n");
+	do{
+		for(i=0;i<6;i++){
+			mostrarMenu(pergunta, acertos, erros, i);
+			for(j=i*4;j<i*4+4;j++){
+				printf("%d - ",j-i*4+1);
+				printf("%s\n", opcoes[j]);	
+			}
+			printf("Escolha uma opção: ");
+			scanf("%d",&resposta);
+			if(strcmp(opcoes[resposta+i*4-1], opcaocorreta[i])== 0){
+				printf("Resposta esta correta!!\n");
+				acertos++;
+			}
+			else{
+				printf("Resposta esta incorreta!!!!\n");
+				printf("A opçao correta é %s\n",opcaocorreta[i]);
+				erros++;
+			}
+			system("pause");
+			system("cls");
+		}
+		printf("\n========== FIM DO QUIZ ==========\n");
 		printf("Acertos: %d\n",acertos);
 		printf("Erros: %d\n",erros);
-		printf("Pergunta %d\n\n",i+1);
-		printf("%s\n", pergunta[i]);
-		for(j=i*4;j<i*4+4;j++){
-			printf("%d - ",j-i*4+1);
-			printf("%s\n", opcoes[j]);	
-		}
-		printf("Escolha uma opção: ");
-		scanf("%d",&resposta);
-		if(strcmp(opcoes[resposta+i*4-1], opcaocorreta[i])== 0){
-			printf("Resposta esta correta!!\n");
-			acertos++;
-		}
-		else{
-			printf("Resposta esta incorreta!!!!\n");
-			erros++;
-		}
-		system("pause");
+		printf("%% de Acertos: %.2f%%\n", (acertos * 100.0) / 6);
+		printf("Quer repetir?  (s/n): ");
+		scanf(" %c",&repetir);
 		system("cls");
-	}
-getchar();
+		erros=0;
+		acertos=0;
+	}while(repetir=='s');
+	printf("Obrigado por jogar! Volte sempre!\n");
+getch();
 }
